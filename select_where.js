11e7e5ad833ea5623ca594919923ded121b591a6
su1aimxn <sulaiman.778v2@gmail.com>
2024-07-03 11:27:29 +0700

    first commit

---
+++ select_where.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "mydb",
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+    var sql = "SELECT * FROM Customers WHERE address = 'Highway 37'";
+
+    con.query(sql, function(err, result, fields) {
+        if (err) throw err;
+        console.log(result); 
+    });
+
+});
